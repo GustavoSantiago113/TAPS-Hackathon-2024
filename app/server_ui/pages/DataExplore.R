87a1d3c65bef60dbbee43cacfe621cf38477cd61
Gustavo N. Santiago <104911112+GustavoSantiago113@users.noreply.github.com>
2024-10-29 20:54:11 -0500

    NDVI map and page layout changes

--- app/server_ui/pages/DataExplore.R
+++ app/server_ui/pages/DataExplore.R
@@ -6,17 +6,36 @@ data_explore <- function(){
     div(
       class = "page",
       div(
-          h3(
-              "Click on a plot to see its data"
-            ),
-          div(
-            class = "map-box",
-            leafletOutput("map", width = "350px", height = "450px") %>% withSpinner(color="#512888"),
-            uiOutput(outputId = "infoBox"),
-            "Graph"
+        class = "outputs",
+        br(),
+        sliderInput(
+          inputId = "dateInput",
+          label = "Change the day of the year",
+          min = 92,
+          max = 289,
+          value = 92,
+          step = 1
+        ),
+        h3(
+            "Click on a plot to see the data"
           ),
+        div(
+          class = "map-box",
+          leafletOutput("map", width = "550px", height = "350px") %>% withSpinner(color="#512888"),
+          uiOutput(outputId = "infoBox"),
+         ),
+        br(),
+        "NDVI Graph",
+        br(),
+        "Plot 2"
       ),
-      uiOutput(outputId = "staticCards")
+      div(
+        class = "output-right",
+        br(),
+        uiOutput(outputId = "staticCards"),
+        br(),
+        "Plot 4"
+      )
     )
   )
 }
